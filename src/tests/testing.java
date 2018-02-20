34551d90395ca77ebda627d165164f0e561c54f5
Josh Gerena <jngerena@users.noreply.github.com>
2018-02-20 14:31:34 -0500

    completed first two tests

--- src/tests/testing.java
+++ src/tests/testing.java
@@ -10,20 +10,22 @@ import java.util.List;
 import org.junit.Test;
 
 import code.Board;
+import code.Location;
 
 public class testing
 {
 	@Test
 	public void Testlocations()
 	{
-		Board test = new Board(Location[5][5]);
+		
+		Board test = new Board(5,5);
 		Location[][] newBoard = test.getBoard();
 		assertEquals(5, newBoard.length);
 		assertEquals(5, newBoard[0].length);
 	}
 	@Test
 	public void TestReadCodenames() {
-		Board test=new Board();
+		Board test=new Board(5,5);
 		test.setCodeNames("src/GameWords1.txt");
 		List<String> testList=Arrays.asList("AFRICA" , "AGENT" , "AIR" , "ALIEN" , "ALPS" , "AMAZON" , "AMBULANCE" , "AMERICA" , "ANGEL" , "ANTARCTICA" , "APPLE" , "ARM" , "ATLANTIS" , "AUSTRALIA" , "AZTEC" , 
 				"BACK" , 
@@ -43,9 +45,7 @@ public class testing
 				"BELT");
 		
 		List<String> response=test.getCodeNames();
-		for(int i=0; i<response.size(); i++) {
-			assertEquals("")
-		}
+			assertEquals("codename files match", testList,response);
 		
 	}
 	
