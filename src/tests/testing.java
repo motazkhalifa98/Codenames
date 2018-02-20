659f41da3d314089c59b51458307a622a09cda51
Josh Gerena <jngerena@users.noreply.github.com>
2018-02-20 14:22:32 -0500

    added codenames and another test method

--- src/tests/testing.java
+++ src/tests/testing.java
@@ -4,15 +4,19 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Test;
+
+import code.Board;
 
 public class testing
 {
 	@Test
 	public void Testlocations()
 	{
-		Board test = new Board();
+		Board test = new Board(Location[5][5]);
 		Location[][] newBoard = test.getBoard();
 		assertEquals(5, newBoard.length);
 		assertEquals(5, newBoard[0].length);
@@ -20,7 +24,29 @@ public class testing
 	@Test
 	public void TestReadCodenames() {
 		Board test=new Board();
-		test.setCodeNames(GameWords.txt);
+		test.setCodeNames("src/GameWords1.txt");
+		List<String> testList=Arrays.asList("AFRICA" , "AGENT" , "AIR" , "ALIEN" , "ALPS" , "AMAZON" , "AMBULANCE" , "AMERICA" , "ANGEL" , "ANTARCTICA" , "APPLE" , "ARM" , "ATLANTIS" , "AUSTRALIA" , "AZTEC" , 
+				"BACK" , 
+				"BALL" , 
+				"BAND" , 
+				"BANK" , 
+				"BAR" , 
+				"BARK" , 
+				"BAT" , 
+				"BATTERY" , 
+				"BEACH" , 
+				"BEAR" , 
+				"BEAT" , 
+				"BED" , 
+				"BEIJING" , 
+				"BELL" , 
+				"BELT");
+		
+		List<String> response=test.getCodeNames();
+		for(int i=0; i<response.size(); i++) {
+			assertEquals("")
+		}
+		
 	}
 	
 }
