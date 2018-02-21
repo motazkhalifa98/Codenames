1a20c251b314bb4b8d8c26965e6ade3072ac2984
Josh Gerena <jngerena@users.noreply.github.com>
2018-02-20 20:56:14 -0500

    finished third test case and added code to pass

--- src/code/Board.java
+++ src/code/Board.java
@@ -4,11 +4,13 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Board {
 	
 	private Location[][] board;
+	private List<String> fullCodenames=new ArrayList<String>();
 	private List<String> codenames=new ArrayList<String>();
 	public Board(int x, int y) {
 		Location[][] thisboard = new Location[x][y];
@@ -29,15 +31,22 @@ public class Board {
 	public void setCodeNames(String filename) {
 		try {
 			for(String line : Files.readAllLines(Paths.get(filename))){
-				System.out.println(line);
-			    codenames.add(line);
+				fullCodenames.add(line);
 }
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		Collections.shuffle(fullCodenames);
+		for(int name=0; name<fullCodenames.size(); name++) {
+			codenames.add(fullCodenames.get(name));
+		}
+		
 	}
-	public List<String> getCodeNames(){
+	public List<String> getAllCodeNames(){
+		return fullCodenames;
+	}
+	public List<String> getGameCodeNames(){
 		return codenames;
 	}
 	
--- src/tests/testing.java
+++ src/tests/testing.java
@@ -1,7 +1,9 @@
 package tests;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -15,38 +17,85 @@ import code.Location;
 public class testing
 {
 	@Test
-	public void Testlocations()
+	public void Test25Locations()
 	{
 		
 		Board test = new Board(5,5);
 		Location[][] newBoard = test.getBoard();
-		assertEquals(5, newBoard.length);
-		assertEquals(5, newBoard[0].length);
+		assertEquals("board size",5, newBoard.length);
+		assertEquals("board size",5, newBoard[0].length);
 	}
 	@Test
-	public void TestReadCodenames() {
+	public void TestReadCodenameFile() {
 		Board test=new Board(5,5);
-		test.setCodeNames("src/GameWords1.txt");
-		List<String> testList=Arrays.asList("AFRICA" , "AGENT" , "AIR" , "ALIEN" , "ALPS" , "AMAZON" , "AMBULANCE" , "AMERICA" , "ANGEL" , "ANTARCTICA" , "APPLE" , "ARM" , "ATLANTIS" , "AUSTRALIA" , "AZTEC" , 
-				"BACK" , 
-				"BALL" , 
-				"BAND" , 
-				"BANK" , 
-				"BAR" , 
-				"BARK" , 
-				"BAT" , 
-				"BATTERY" , 
-				"BEACH" , 
-				"BEAR" , 
-				"BEAT" , 
-				"BED" , 
-				"BEIJING" , 
-				"BELL" , 
-				"BELT");
+		test.setCodeNames("src/GameWords.txt");
+		List<String> testList=Arrays.asList("area",
+				"book",
+				"business",
+				"case",
+				"child",
+				"company",
+				"country",
+				"day",
+				"eye",
+				"fact",
+				"family",
+				"government",
+				"group",
+				"hand",
+				"home",
+				"job",
+				"life",
+				"lot",
+				"man",
+				"money",
+				"month",
+				"mother",
+				"Mr",
+				"night",
+				"number",
+				"part",
+				"people",
+				"place",
+				"point",
+				"problem",
+				"program",
+				"question",
+				"right",
+				"room",
+				"school",
+				"state",
+				"story",
+				"student",
+				"study",
+				"system",
+				"thing",
+				"time",
+				"water",
+				"way",
+				"week",
+				"woman",
+				"word",
+				"work",
+				"world",
+				"year");
 		
-		List<String> response=test.getCodeNames();
-			assertEquals("codename files match", testList,response);
-		
+		List<String> response=test.getAllCodeNames();
+		for(int word=0; word<response.size(); word++) {
+			assertTrue("codename files match", testList.contains(response.get(word)));
+		}
+	}
+	@Test
+	public void test25uniqueCodenames() {
+		Board test=new Board(5,5);
+		test.setCodeNames("src/GameWords.txt");
+		List<String> actual=test.getGameCodeNames();
+		for(int name=0; name<actual.size(); name++) {
+			for(int code=name+1; code<actual.size(); code++) {
+				assertTrue(actual.get(name)!=actual.get(code));
+			}
+			
+		}
 	}
 	
 }
