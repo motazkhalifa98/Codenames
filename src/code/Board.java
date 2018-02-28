29e231c263fa815b82226bb1a042a19698efa02a
Motaz Khalifa <Motaz Khalifa@LAPTOP-IPBM1LRF>
2018-02-28 18:21:01 -0500

    wdw

--- src/code/Board.java
+++ src/code/Board.java
@@ -8,11 +8,11 @@ import java.util.Collections;
 import java.util.List;
 
 public class Board {
-	
+
 	private String CLUE = "this is a clue";
-	
+
 	private String currentPlayer = "Red";
-	
+
 	private Location[][] board;
 	private List<String> fullCodenames=new ArrayList<String>();
 	private List<String> codenames=new ArrayList<String>();
@@ -20,7 +20,7 @@ public class Board {
 	public Board(int x, int y) {
 		Location[][] thisboard = new Location[x][y];
 		this.board = thisboard;
-		
+
 	}
 
 	public Location[][] getBoard() {
@@ -35,7 +35,7 @@ public class Board {
 		try {
 			for(String line : Files.readAllLines(Paths.get(filename))){
 				fullCodenames.add(line);
-}
+			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -44,7 +44,7 @@ public class Board {
 		for(int name=0; name<25; name++) {
 			codenames.add(fullCodenames.get(name));
 		}
-		
+
 	}
 	public List<String> getAllCodeNames(){
 		return fullCodenames;
@@ -53,46 +53,48 @@ public class Board {
 		return codenames;
 	}
 	private ArrayList<Person> persons = new ArrayList<Person>();
-//j
+	//j
 	public void makeList() {
 		int j = 0;
-		
+
 		for (int i=0; i <9; i++) {
-			RedAgent red = new RedAgent(codenames.get(i));
+			RedAgent red = new RedAgent();
 			j = i;
 			persons.add(red);
 		}
 		for (int x=0; x< 8; x++) {
-			BlueAgent blue = new BlueAgent(codenames.get(x+j));
+			BlueAgent blue = new BlueAgent();
 			j = x+j;
 			persons.add(blue);
 		}
 		for (int y=0; y< 7; y++) {
-			InnocentBystander innocent = new InnocentBystander(codenames.get(y+j));
+			InnocentBystander innocent = new InnocentBystander();
 			j = j+y;
 			persons.add(innocent);
 		}
-		Assassin assassin = new Assassin("G");
+		Assassin assassin = new Assassin();
 		persons.add(assassin);
 		Collections.shuffle(persons);
 		Location[][] bleh = new Location[5][5];
-		
-		for (int i = 0; i < 5; i++) {
-			for (int k = 0; k < bleh[i].length; k++) {
-				// want to loop inside it and set every person's codename to a codename from the arraylist of perosns
-				for (int m = 0; m < 25; m++) {
-				bleh[i][k].setCodeName(codenames.get(m));
+
+		for (int i =0; i <5; i++) {
+			for (int k =0; k < bleh[i].length; k++) {
+				for (Person type : persons) {
+					bleh[i][k].setPersonType(type);
 				}
-				for (int g =0; g < 25; g++) {
-				bleh[i][k].setPersonType(persons.get(g));
 			}
 		}
+
+		for (int i =0; i <5; i++) {
+			for (int k =0; k < bleh[i].length; k++) {
+				for (String code : codenames) {
+					bleh[i][k].setCodeName(code);
+				}
+			}
 		}
-		
+
 		setBoard(bleh);
-	
-	
-	
+
 	}
 	public String getClue() {
 		return CLUE;
@@ -108,22 +110,22 @@ public class Board {
 	}
 	public void legalClue(String clue) {
 		a:
-		for (int i=0; i<board.length; i++) {
-			for(int k=0; k<board[0].length; k++) {
-				if (CLUE.equals(board[i][k].getCodeName()) && board[i][k].Reveal == 0) {
-					//forfet turn
-					if (getCurrentPlayer().equals("Red")) {
-						setCurrentPlayer("Blue");
+			for (int i=0; i<board.length; i++) {
+				for(int k=0; k<board[0].length; k++) {
+					if (CLUE.equals(board[i][k].getCodeName()) && board[i][k].Reveal == 0) {
+						//forfet turn
+						if (getCurrentPlayer().equals("Red")) {
+							setCurrentPlayer("Blue");
+						}
+						else {
+							setCurrentPlayer("RED");
+						}
+						System.out.println("turn has been forfeted, it is " + getCurrentPlayer() + "'s turn");
+						break a;
 					}
-					else {
-						setCurrentPlayer("RED");
-					}
-					System.out.println("turn has been forfeted, it is " + getCurrentPlayer() + "'s turn");
-					break a;
 				}
 			}
-		}
-		System.out.println(getCurrentPlayer() + " this is your clue: " + CLUE);
+	System.out.println(getCurrentPlayer() + " this is your clue: " + CLUE);
 	}
 	public void makeMove(int row, int col) {
 		if (board[row][col].Reveal == 0) {
@@ -133,7 +135,7 @@ public class Board {
 			//reveal the location
 			if (currentPlayer.equals("Red")) {
 				board[row][col].setReveal(1);
-				
+
 			}
 			else if(currentPlayer.equals("Blue")) {
 				board[row][col].setReveal(1);
@@ -142,15 +144,15 @@ public class Board {
 				System.out.println("code error somewhere in makeMove()");
 			}
 		}
-		
+
 	}
 	public void checkGameState(int rol, int col) {
 		if (board[row][col].getPersonType() == ) {
-			
+
 		}
 	}
 	public void win() {
-		
+
 	}
 	public void main(String[] args) {
 		board = new Location[5][5];
