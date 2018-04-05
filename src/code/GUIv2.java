bde537c5483c32760c3ce165b6b4fadd9d827f7b
rly2 <rly2@UBPS-CPN3S-W62.itorg.ad.buffalo.edu>
2018-04-05 19:56:30 -0400

    kill me

--- src/code/GUIv2.java
+++ src/code/GUIv2.java
@@ -15,19 +15,31 @@ import java.util.ArrayList;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class GUIv2 extends JFrame{
+	
+	boolean gameRunning = false;
+	static String[] clue = new String[2];
+	static String[] options2 = {"OK"};
+	String[] currentTurn = {"SpyMaster", "Red", "Blue"};
 	
 	public static void main(String[] args){
 		//This creates a app window
 		//menu screen
     	JFrame frame = new JFrame("I Hate CSE");
+    	frame.setLayout(new BorderLayout());
     	JPanel contrPanel = new JPanel();
     //Menu
     	JPanel panel1 = new JPanel();
     //Game
+    	
+// a = main menu b = end turn c = 25 buttons
     //2 is player red
     	JPanel panel2 = new JPanel();
     	JPanel panel2a = new JPanel();
@@ -40,7 +52,12 @@ public class GUIv2 extends JFrame{
     	JPanel panel3c = new JPanel();
     //Bonus and other shit
     	JPanel panel4 = new JPanel();
-    	
+    //5 is SpyMaster
+    	JPanel panel5 = new JPanel();
+    	JPanel panel5a = new JPanel();
+    	JPanel panel5c = new JPanel();
+    	JLabel lbl = new JLabel("Clue:");
+    	JTextField txt = new JTextField(10);
     	//size screen (x, y)
     	frame.setSize(600, 500);
     	
@@ -50,7 +67,7 @@ public class GUIv2 extends JFrame{
     	label1.setFont(new Font("Serif", Font.BOLD, 50));
     	
     	JOptionPane.showMessageDialog(frame, "I hate cse please give me a 100", "PLEASE READ", JOptionPane.WARNING_MESSAGE);
-
+    	
     	CardLayout cl = new CardLayout();
     	contrPanel.setLayout(cl);
     	panel2.setBackground(Color.RED);
@@ -63,10 +80,11 @@ public class GUIv2 extends JFrame{
     	contrPanel.add(panel2, "2");
     	contrPanel.add(panel3, "3");
     	contrPanel.add(panel4, "4");
+    	contrPanel.add(panel5, "5");
     	cl.show(contrPanel, "1");
-    	
+ ////////////////////////////
     	String[] options = {"Yes", "No"};
-
+ ////////////////////////////
     	ArrayList<JButton> jbuttonList1 = new ArrayList<JButton>();
     	for(int i = 1; i<26; i++){
     		JButton b = new JButton("Button " + i);
@@ -108,6 +126,11 @@ public class GUIv2 extends JFrame{
 	//note: D refers to the panel4 (bonus screen)
 		JButton buttonD1 = new JButton("Main Menu");
 		buttonD1.setPreferredSize(new Dimension(100, 30));
+	//note: E refers to the panel5 (game screen SPYMASTER)
+		JButton buttonE1 = new JButton("Main Menu");
+		JButton buttonE2 = new JButton("End Turn");
+		buttonE1.setPreferredSize(new Dimension(100, 30));
+		buttonE2.setPreferredSize(new Dimension(100, 30));
 	//note: Panel1
 		//this adds a "action" to a button (when the button is pressed, run this)
 		buttonA1.addActionListener(new ActionListener(){
@@ -116,7 +139,28 @@ public class GUIv2 extends JFrame{
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
 				//start the game
-				cl.show(contrPanel, "2");
+				cl.show(contrPanel, "5");
+			    boolean blueClue = false;
+				boolean redClue = false;
+//				while(!redClue){
+					System.out.println("try again");
+					int redInput = JOptionPane.showOptionDialog(null, panel5, "The Title", JOptionPane.NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options2 , options2[0]);
+					//check legal
+//					if(redInput==0){
+//						String text = txt1.getText();
+//						clue[0]=text;
+//					}
+//				}
+//				while(!blueClue){
+//					System.out.println("try again");
+//					int blueInput = JOptionPane.showOptionDialog(null, "blue", "title", JOptionPane.QUESTION_MESSAGE, JOptionPane.QUESTION_MESSAGE, options2 , options2[0]);
+//					//check legal
+//					if(blueInput==0){
+//						String text = txt2.getText();
+//						clue[1]=text;
+//					}
+//				}
+//				cl.show(contrPanel, "2");
 			}
 			
 		});
@@ -138,7 +182,10 @@ public class GUIv2 extends JFrame{
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
 				//it exits
-				System.exit(0);
+				int thing = JOptionPane.showOptionDialog(null, "Do you want to exit the app?", "Exit Conformation", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
+				if(thing == 0 && thing != -1){
+					System.exit(0);
+				}
 			}
 		});
 	//note: Panel2
@@ -200,11 +247,44 @@ public class GUIv2 extends JFrame{
 				cl.show(contrPanel, "1");
 			}
 		});
+	//note: panel5
+		buttonE1.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				cl.show(contrPanel, "1");
+			}
+			
+		});
+	//menu stuff
+		JMenuBar menuBar = new JMenuBar();
+        JMenu menu1 = new JMenu("FILE");
+        menuBar.add(menu1);
+        JMenuItem startItem = new JMenuItem("Start");
+        JMenuItem quitItem = new JMenuItem("Quit");
+        menu1.add(startItem);
+        menu1.add(quitItem);
+        startItem.addActionListener(new ActionListener() {
+        	
+        	@Override
+        	public void actionPerformed(ActionEvent event) {
+        		cl.show(contrPanel, "2");
+        	}
+        });
+        quitItem.addActionListener(new ActionListener() {
+        	
+    		@Override
+    		public void actionPerformed(ActionEvent event) {
+    			System.exit(0);
+    		}
+        });
 //ignore
 //		this just places it left to right (assuming you have more than a 2 by 2)
 		panel2b.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
 		panel3b.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
 		frame.add(contrPanel);
+		frame.add(menuBar, BorderLayout.NORTH);
 		panel1.add(label1);
 		panel1.add(buttonA1);
 		panel1.add(buttonA2);
@@ -229,7 +309,12 @@ public class GUIv2 extends JFrame{
 		}
 		panel3c.add(buttonC2);
 		
-		panel4.add(buttonD1);
+		panel4.add(buttonD1);	
+		
+		panel5.add(panel5a);
+		panel5a.add(buttonE1);
+		panel5.add(lbl);
+		panel5.add(txt);
 		
 		//set visible.... idek... you got to make it visible so you can see???
 		frame.setVisible(true);
