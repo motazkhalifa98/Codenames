3023eb358b43cc406ed788f1d51b6aa7f3e07ef4
rly2 <rly2@UBPS-CPN3S-W26.itorg.ad.buffalo.edu>
2018-04-09 19:33:19 -0400

    small clean up

--- src/code/SpyMasterPanel.java
+++ src/code/SpyMasterPanel.java
@@ -2,12 +2,18 @@ package code;
 
 import java.awt.CardLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -17,13 +23,12 @@ public class SpyMasterPanel extends JPanel implements KeyListener{
 	private JTextField hintField;
 	private JPanel mainPanel;
 	private CardLayout cl;
-	private boolean gameRunning;
 	
 	public SpyMasterPanel(JPanel mainPanel, CardLayout cl) {
 		// TODO Auto-generated constructor stub
 		this.mainPanel = mainPanel;
 		this.cl = cl;
-		this.gameRunning = gameRunning;
+		setLayout(new GridBagLayout());
 		
 		setLayout(new GridBagLayout());
 		setBackground(Color.ORANGE);
@@ -32,7 +37,27 @@ public class SpyMasterPanel extends JPanel implements KeyListener{
 		hintLabel.setFont(new Font("Serif", Font.BOLD, 40));
 		hintField.setFont(new Font ("Arial", Font.BOLD, 35));
 		hintField.addKeyListener(this);
+		
+		JLabel label = new JLabel("Input your hint");
+		label.setFont(new Font("Ariel", Font.ITALIC, 40));
+		JPanel locationButtons = new JPanel();
+		locationButtons.setBackground(Color.ORANGE);
+		locationButtons.setLayout(new GridLayout(5, 5, 10, 10));
+    	ArrayList<JButton> jbuttonList = new ArrayList<JButton>();
+    	for(int i = 1; i<26; i++){
+    		JButton b = new JButton("Button " + i);
+    		b.setEnabled(false);
+    		//stuff
+    		
+    		
+    		b.setPreferredSize(new Dimension(100, 50));
+    		jbuttonList.add(b);
+    	}
+		
 		GridBagConstraints gc = new GridBagConstraints();
+		for(int i=0; i<jbuttonList.size(); i++){
+			locationButtons.add(jbuttonList.get(i));
+		}
 		gc.gridx = 0;
 		gc.gridy = 0;
 		add(hintLabel, gc);
@@ -40,13 +65,18 @@ public class SpyMasterPanel extends JPanel implements KeyListener{
 		gc.gridx = 1;
 		gc.gridy = 0;
 		add(hintField, gc);
+		
+		gc.gridx = 0;
+		gc.gridy = 2;
+		gc.gridwidth = 2;
+		add(locationButtons, gc);
+		
 	}
 
 	private void checkLegal() {
 		// TODO Auto-generated method stub
 		System.out.println(hintField.getText());
 		if(hintField.getText().equals("legal")) {
-			gameRunning = true;
 			cl.show(mainPanel, "4");
 		}
 	}
@@ -55,7 +85,6 @@ public class SpyMasterPanel extends JPanel implements KeyListener{
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
 		if(e.getKeyCode() == KeyEvent.VK_ENTER) {
-			gameRunning = false;
 			checkLegal();
 		}
 	}
